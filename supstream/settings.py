8e074d307823484c03b153b5c1f50ac8c24dede8
Yaniv Aknin <yaniv@aknin.name>
2012-01-02 08:52:36 +0200

    add automagical RELEASE_ID that falls back to random
    Currently, if it decides your project is git based, than rather than
    using a random RELEASE_ID it will use the ref(HEAD)+md5(git diff).
    
    Similar constructs can probably be used with most other SCMs, and maybe
    even with md5(recursive mtime) or so.

--- supstream/settings.py
+++ supstream/settings.py
@@ -1,10 +1,10 @@
-import uuid
 import os
 
 from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 
-RELEASE_ID=uuid.uuid4().hex
+from .utils import automagical_release_id, AutomagicalReleaseIDError
+
 
 STATIC_URL=getattr(settings, 'STATIC_URL', None)
 if not STATIC_URL:
@@ -14,3 +14,10 @@ STATIC_ROOT=getattr(settings, 'STATIC_ROOT', None)
 if not STATIC_ROOT or not os.path.isdir(STATIC_ROOT):
     raise ImproperlyConfigured("django-static-upstream requires you to point STATIC_ROOT at "
                                "the location of your statics")
+
+try:
+    RELEASE_ID=getattr(settings, 'RELEASE_ID', automagical_release_id(STATIC_ROOT))
+except AutomagicalReleaseIDError, error:
+    # if you caught this, you should either provide a RELEASE_ID setting in your settings.py, as the docs say
+    # OR file a bug report, because the automagical_release_id() mechanism is Supposed To Always Work (tm)
+    raise ImproperlyConfigured("django-static-upstream failed getting you an automatic release id: %s" % (error,))
--- supstream/utils.py
+++ supstream/utils.py
@@ -1,4 +1,11 @@
+import os
+import uuid
+import logging
 from hashlib import md5
+from subprocess import Popen, PIPE
+
+logger = logging.getLogger(__name__)
+logger.error('foo!')
 
 # ideas for optimizing this abound, but for now it's Good Enough
 def file_digest(filename, return_contents=False):
@@ -10,3 +17,36 @@ def file_digest(filename, return_contents=False):
             if return_contents:
                 contents.append(block)
     return context.hexdigest(), "".join(contents)
+
+def find_upwards(filename, cwd=None):
+    cwd = cwd or os.getcwd()
+    while True:
+        if os.path.exists(os.path.join(cwd, filename)):
+            return os.path.abspath(cwd)
+        if os.path.abspath(cwd) == '/':
+            raise EnvironmentError("unable to find %s" % (filename,))
+        cwd = os.path.join(cwd, os.pardir)
+
+class AutomagicalReleaseIDError(Exception):
+    pass
+
+def automagical_release_id(path):
+    def git_handler(path):
+        gitdir = find_upwards('.git', path)
+        with open('/dev/null', 'w') as devnull:
+            process = Popen('git rev-parse HEAD ; git diff | md5', cwd=gitdir, shell=True,
+                            stdout=PIPE, stderr=PIPE, stdin=devnull)
+            stdout, stderr = process.communicate()
+            if process.returncode != 0:
+                raise AutomagicalReleaseIDError('git handler received nonzero exit; stderr=%r' % (stderr,))
+            return 'GIT-' + stdout.strip().replace('\n', '-')
+
+    for scm_handler in (git_handler,):
+        try:
+            return scm_handler(path)
+        except EnvironmentError:
+            pass
+    else:
+        # fallback to random release id; this will force rehashing of served statics every time you reload code
+        logger.warn('falling back to random release id; provide RELEASE_ID in settings to avoid this')
+        return 'UUID-' + uuid.uuid4().hex
