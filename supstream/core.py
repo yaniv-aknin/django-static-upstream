6b829317db73fa1ca567711a4f0ae9eef9c09d7b
Yaniv Aknin <yaniv@aknin.name>
2012-07-03 18:39:10 +0300

    core: eliminate an idiotic race condition

--- supstream/core.py
+++ supstream/core.py
@@ -14,7 +14,7 @@ def get_url_for_static_file(filename):
 
     cache_key = 'supstream_static-%s-%s' % (settings.RELEASE_ID, filename)
     digest = cache.get(cache_key)
-    if cache.get(cache_key) is None:
+    if digest is None:
         digest, _ = file_digest(os.path.join(settings.STATIC_ROOT, filename))
         cache.set(cache_key, digest)
     return urljoin(settings.STATIC_URL, '!%s/%s' % (digest, filename))
